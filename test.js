18b619c9fbe6bd8620157257222919ae101af83d
Kyle E. Mitchell <kyle@kemitchell.com>
2016-07-26 11:22:30 -0700

    Add tests

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 var merge = require('./')
 var tape = require('tape')
 
-tape(function (test) {
+tape('concatenates independent dependencies', function (test) {
   test.deepEqual(
     merge(
       [{name: 'a', version: '1.0.0', links: []}],
@@ -10,6 +10,45 @@ tape(function (test) {
     [
       {name: 'a', version: '1.0.0', links: []},
       {name: 'b', version: '1.0.0', links: []}
+    ]
+  )
+  test.end()
+})
+
+tape('merges shared dependencies', function (test) {
+  test.deepEqual(
+    merge(
+      [
+        {name: 'a', version: '1.0.0', links: []},
+        {name: 'b', version: '1.0.0', links: []},
+        {name: 'c', version: '1.0.0', links: []}
+      ],
+      [
+        {name: 'c', version: '1.0.0', links: []},
+        {name: 'd', version: '1.0.0', links: []},
+        {name: 'e', version: '1.0.0', links: []}
+      ]
+    ),
+    [
+      {name: 'a', version: '1.0.0', links: []},
+      {name: 'b', version: '1.0.0', links: []},
+      {name: 'c', version: '1.0.0', links: []},
+      {name: 'd', version: '1.0.0', links: []},
+      {name: 'e', version: '1.0.0', links: []}
+    ]
+  )
+  test.end()
+})
+
+tape('does not merge distinct versions', function (test) {
+  test.deepEqual(
+    merge(
+      [{name: 'a', version: '1.0.0', links: []}],
+      [{name: 'a', version: '1.0.1', links: []}]
+    ),
+    [
+      {name: 'a', version: '1.0.0', links: []},
+      {name: 'a', version: '1.0.1', links: []}
     ]
   )
   test.end()
