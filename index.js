2e7266ddad07c39144366951469c27e5289ab026
Kyle E. Mitchell <kyle@kemitchell.com>
2016-08-05 16:43:08 -0700

    Resolve missing dependencies

--- index.js
+++ index.js
@@ -1,28 +1,139 @@
+var find = require('array-find')
+var semver = require('semver')
+
 module.exports = function (firstTree, secondTree) {
-  secondTree.forEach(function (secondElement) {
-    var matchingDependencyIndex = firstTree.findIndex(function (have) {
-      return (
-        have.name === secondElement.name &&
-        have.version === secondElement.version
-      )
+  secondTree.forEach(function (newDependency) {
+    var alreadyHave = firstTree.some(function (existingDependency) {
+      return equivalents(existingDependency, newDependency)
     })
-    if (matchingDependencyIndex === -1) {
-      firstTree.push(secondElement)
-    } else if (secondElement.range !== undefined) {
-      var firstElement = firstTree[matchingDependencyIndex]
-      if (firstElement.range === undefined) {
-        firstElement.range = secondElement.range
-      } else {
-        if (firstElement.range !== secondElement.range) {
-          var error = new Error(
-            'direct-dependency range mismatch ' +
-            'for ' + secondElement.name + '@' + secondElement.version
-          )
-          error.rangeMismatch = true
-          throw error
+    if (!alreadyHave) {
+      firstTree.push(newDependency)
+    }
+  })
+  resolveMissingIndirectDependencies(firstTree)
+  throwForDirectDependencyRangeConflict(firstTree)
+  removeSupersededDependencies(firstTree)
+}
+
+function equivalents (a, b) {
+  return (
+    a.name === b.name &&
+    a.version === b.version &&
+    a.range === b.range
+  )
+}
+
+function resolveMissingIndirectDependencies (tree) {
+  tree.forEach(function (dependency) {
+    dependency.links.forEach(function (link) {
+      var name = link.name
+      var range = link.range
+      var version = link.version
+      if (version === undefined) {
+        var satisfying = maxSatisfying(tree, name, range)
+        if (satisfying) {
+          link.version = satisfying.version
         }
+      }
+    })
+  })
+}
+
+function throwForDirectDependencyRangeConflict (tree) {
+  tree.forEach(function (dependency) {
+    var name = dependency.name
+    // Check for direct-dependency range conflicts.
+    if (dependency.range !== undefined) {
+      var hasDirectConflict = tree.some(function (otherDependency) {
+        return (
+          otherDependency.name === name &&
+          otherDependency.range !== undefined &&
+          otherDependency.range !== dependency.range
+        )
+      })
+      if (hasDirectConflict) {
+        var error = new Error(
+          'direct-dependency range mismatch for ' + name
+        )
+        error.rangeConflict = true
+        throw error
       }
     }
   })
-  return firstTree
+}
+
+function removeSupersededDependencies (tree) {
+  tree.forEach(function (dependency, index) {
+    if (dependency.range) {
+      var name = dependency.name
+      var range = dependency.range
+      var superseding
+      // Check missing direct dependencies.
+      if (dependency.version === undefined) {
+        superseding = maxSatisfying(tree, name, range)
+        if (superseding) {
+          // Any range conflict that might appear here would have caused
+          // `checkForDirectDependencyRangeConflict` to throw an error.
+          superseding.range = dependency.range
+          spliceOut(tree, dependency)
+        }
+      // Check resolved direct dependencies.
+      } else /* if (dependency.version) */ {
+        var version = dependency.version
+        tree.forEach(function (other) {
+          var superseded = (
+            other.name === name &&
+            other.version === version &&
+            other.range === undefined
+          )
+          if (superseded) {
+            spliceOut(tree, other)
+          }
+        })
+      }
+    }
+  })
+}
+
+function maxSatisfying (tree, name, range) {
+  var candidates = []
+  tree.forEach(function (dependency) {
+    var match = (
+      dependency.name === name &&
+      dependency.version !== null &&
+      satisfiesRange(dependency.version, range)
+    )
+    if (match) {
+      candidates.push(dependency)
+    }
+  })
+  if (candidates.length === 0) {
+    return null
+  } else {
+    var availableVersions = candidates.map(function (dependency) {
+      return dependency.version
+    })
+    var version = semver.maxSatisfying(availableVersions, range)
+    return find(candidates, function (dependency) {
+      return dependency.version === version
+    })
+  }
+}
+
+function satisfiesRange (version, range) {
+  var versionIsSemVer = semver.valid(version) !== null
+  var rangeIsSemVer = semver.validRange(version) !== null
+  if (versionIsSemVer && rangeIsSemVer) {
+    return semver.satisfies(version, range)
+  } else {
+    return version === range
+  }
+}
+
+function spliceOut (array, element) {
+  var index = array.indexOf(element)
+  /* istanbul ignore else */
+  if (index !== -1) {
+    array.splice(index, 1)
+  }
 }
--- test.js
+++ test.js
@@ -2,38 +2,69 @@ var merge = require('./')
 var sort = require('sort-flat-package-tree')
 var tape = require('tape')
 
+var DO_NOT_END = {}
+
+function doMerge (a, b, result, end) {
+  merge(a, b)
+  sort(a)
+  this.deepEqual(a, result)
+  if (end !== DO_NOT_END) {
+    this.end()
+  }
+}
+
 tape('concatenates independent dependencies', function (test) {
-  test.deepEqual(
-    sort(
-      merge(
-        [{name: 'a', version: '1.0.0', links: []}],
-        [{name: 'b', version: '1.0.0', links: []}]
-      )
-    ),
+  doMerge.apply(test, [
+    [
+      {name: 'a', version: '1.0.0', links: []}
+    ],
+    [
+      {name: 'b', version: '1.0.0', links: []}
+    ],
     [
       {name: 'a', version: '1.0.0', links: []},
       {name: 'b', version: '1.0.0', links: []}
     ]
-  )
-  test.end()
+  ])
+})
+
+tape('preserves indirect dependencies', function (test) {
+  doMerge.apply(test, [
+    [
+      {
+        name: 'a',
+        version: '1.0.0',
+        links: [{name: 'b', range: '^1.0.0', version: '1.0.0'}]
+      },
+      {name: 'b', version: '1.0.0', links: []}
+    ],
+    [
+      {name: 'c', version: '1.0.0', links: []}
+    ],
+    [
+      {
+        name: 'a',
+        version: '1.0.0',
+        links: [{name: 'b', range: '^1.0.0', version: '1.0.0'}]
+      },
+      {name: 'b', version: '1.0.0', links: []},
+      {name: 'c', version: '1.0.0', links: []}
+    ]
+  ])
 })
 
 tape('merges shared dependencies', function (test) {
-  test.deepEqual(
-    sort(
-      merge(
-        [
-          {name: 'a', version: '1.0.0', links: []},
-          {name: 'b', version: '1.0.0', links: []},
-          {name: 'c', version: '1.0.0', links: []}
-        ],
-        [
-          {name: 'c', version: '1.0.0', links: []},
-          {name: 'd', version: '1.0.0', links: []},
-          {name: 'e', version: '1.0.0', links: []}
-        ]
-      )
-    ),
+  doMerge.apply(test, [
+    [
+      {name: 'a', version: '1.0.0', links: []},
+      {name: 'b', version: '1.0.0', links: []},
+      {name: 'c', version: '1.0.0', links: []}
+    ],
+    [
+      {name: 'c', version: '1.0.0', links: []},
+      {name: 'd', version: '1.0.0', links: []},
+      {name: 'e', version: '1.0.0', links: []}
+    ],
     [
       {name: 'a', version: '1.0.0', links: []},
       {name: 'b', version: '1.0.0', links: []},
@@ -41,64 +72,176 @@ tape('merges shared dependencies', function (test) {
       {name: 'd', version: '1.0.0', links: []},
       {name: 'e', version: '1.0.0', links: []}
     ]
-  )
-  test.end()
+  ])
 })
 
 tape('does not merge distinct versions', function (test) {
-  test.deepEqual(
-    sort(
-      merge(
-        [{name: 'a', version: '1.0.0', links: []}],
-        [{name: 'a', version: '1.0.1', links: []}]
-      )
-    ),
+  doMerge.apply(test, [
+    [
+      {name: 'a', version: '1.0.0', links: []}
+    ],
+    [
+      {name: 'a', version: '1.0.1', links: []}
+    ],
     [
       {name: 'a', version: '1.0.0', links: []},
       {name: 'a', version: '1.0.1', links: []}
     ]
-  )
-  test.end()
+  ])
 })
 
 tape('preserves direct dependency ranges', function (test) {
-  var withRange = [
-    {name: 'a', version: '1.0.0', range: '^1.0.0', links: []}
-  ]
-  var withoutRange = [{name: 'a', version: '1.0.0', links: []}]
-  test.deepEqual(sort(merge(withoutRange, withRange)), withRange)
-  test.deepEqual(sort(merge(withRange, withoutRange)), withRange)
-  test.deepEqual(sort(merge(withRange, withRange)), withRange)
+  doMerge.apply(test, [
+    [{name: 'a', version: '1.0.0', links: []}],
+    [{name: 'a', range: '^1.0.0', version: '1.0.0', links: []}],
+    [{name: 'a', range: '^1.0.0', version: '1.0.0', links: []}],
+    DO_NOT_END
+  ])
+  doMerge.apply(test, [
+    [{name: 'a', range: '^1.0.0', version: '1.0.0', links: []}],
+    [{name: 'a', version: '1.0.0', links: []}],
+    [{name: 'a', range: '^1.0.0', version: '1.0.0', links: []}],
+    DO_NOT_END
+  ])
+  doMerge.apply(test, [
+    [{name: 'a', range: '^1.0.0', version: '1.0.0', links: []}],
+    [{name: 'a', range: '^1.0.0', version: '1.0.0', links: []}],
+    [{name: 'a', range: '^1.0.0', version: '1.0.0', links: []}],
+    DO_NOT_END
+  ])
   test.end()
 })
 
 tape('throws for direct-dependency range mismatches', function (test) {
   test.throws(function () {
-    sort(
-      merge(
-        [{name: 'a', version: '1.0.0', range: '^1.0.0', links: []}],
-        [{name: 'a', version: '1.0.0', range: '^1.1.0', links: []}]
-      )
+    merge(
+      [{name: 'a', range: '^1.0.0', version: '1.0.0', links: []}],
+      [{name: 'a', range: '^1.1.0', version: '1.0.0', links: []}]
     )
   }, /direct-dependency range mismatch/)
   test.end()
 })
 
-tape('sorts', function (test) {
-  test.deepEqual(
-    sort(
-      merge(
-        [
-          {name: 'b', version: '1.0.0', links: []},
-          {name: 'a', version: '1.0.0', links: []}
-        ],
-        []
-      )
-    ),
+tape('throws for missing direct range mismatches', function (test) {
+  test.throws(function () {
+    merge(
+      [{name: 'a', range: '^1.0.0', links: []}],
+      [{name: 'a', range: '^1.1.0', links: []}]
+    )
+  }, /direct-dependency range mismatch/)
+  test.end()
+})
+
+tape('merges missing', function (test) {
+  doMerge.apply(test, [
+    [{name: 'a', range: '^1.0.0', links: []}],
+    [{name: 'a', range: '^1.0.0', links: []}],
+    [{name: 'a', range: '^1.0.0', links: []}]
+  ])
+})
+
+tape('resolves missing direct', function (test) {
+  doMerge.apply(test, [
     [
-      {name: 'a', version: '1.0.0', links: []},
+      {name: 'a', range: '^1.0.0', links: []},
+      {name: 'c', range: '^1.0.0', version: '1.0.0', links: []}
+    ],
+    [
+      {name: 'a', range: '^1.0.0', version: '1.0.1', links: []}
+    ],
+    [
+      {name: 'a', range: '^1.0.0', version: '1.0.1', links: []},
+      {name: 'c', range: '^1.0.0', version: '1.0.0', links: []}
+    ]
+  ])
+})
+
+tape('resolves missing indirect', function (test) {
+  doMerge.apply(test, [
+    [
+      {
+        name: 'a',
+        version: '1.0.0',
+        range: '^1.0.0',
+        links: [
+          {name: 'b', range: '^1.0.0'},
+          {name: 'c', range: '^1.0.0'}
+        ]
+      }
+    ],
+    [
+      {name: 'b', version: '1.0.0', links: []}
+    ],
+    [
+      {
+        name: 'a',
+        version: '1.0.0',
+        range: '^1.0.0',
+        links: [
+          {name: 'b', range: '^1.0.0', version: '1.0.0'},
+          {name: 'c', range: '^1.0.0'}
+        ]
+      },
       {name: 'b', version: '1.0.0', links: []}
     ]
-  )
+  ])
+})
+
+tape('engineered for coverage', function (test) {
+  test.throws(function () {
+    merge(
+      [{name: 'a', range: '^1.0.0', links: []}],
+      [{name: 'a', range: '^1.1.0', version: '1.1.0', links: []}]
+    )
+  })
   test.end()
+})
+
+tape('multiple links to missing', function (test) {
+  doMerge.apply(test, [
+    [
+      {
+        name: 'a',
+        version: '1.0.0',
+        range: '^1.0.0',
+        links: [{name: 'd', range: '^1.0.0'}]
+      },
+      {
+        name: 'b',
+        version: '1.0.0',
+        range: '^1.0.0',
+        links: [{name: 'd', range: '^1.1.0'}]
+      },
+      {
+        name: 'c',
+        version: '1.0.0',
+        range: '^1.0.0',
+        links: [{name: 'd', range: '^2.0.0'}]
+      }
+    ],
+    [
+      {name: 'd', version: '1.1.0', links: []}
+    ],
+    [
+      {
+        name: 'a',
+        version: '1.0.0',
+        range: '^1.0.0',
+        links: [{name: 'd', range: '^1.0.0', version: '1.1.0'}]
+      },
+      {
+        name: 'b',
+        version: '1.0.0',
+        range: '^1.0.0',
+        links: [{name: 'd', range: '^1.1.0', version: '1.1.0'}]
+      },
+      {
+        name: 'c',
+        version: '1.0.0',
+        range: '^1.0.0',
+        links: [{name: 'd', range: '^2.0.0'}]
+      },
+      {name: 'd', version: '1.1.0', links: []}
+    ]
+  ])
 })
