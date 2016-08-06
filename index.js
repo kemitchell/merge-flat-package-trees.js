f6846fbe66deebfccefe86bdf21434c4c0773f4e
Kyle E. Mitchell <kyle@kemitchell.com>
2016-08-05 17:06:55 -0700

    WIP: Root object

--- index.js
+++ index.js
@@ -1,28 +1,61 @@
+var find = require('array-find')
+
 module.exports = function (firstTree, secondTree) {
+  mergeRoots(
+    find(firstTree, isRoot),
+    find(secondTree, isRoot)
+  )
   secondTree.forEach(function (secondElement) {
-    var matchingDependencyIndex = firstTree.findIndex(function (have) {
+    var alreadyHas = firstTree.some(function (firstElement) {
       return (
-        have.name === secondElement.name &&
-        have.version === secondElement.version
+        firstElement.name === secondElement.name &&
+        firstElement.missing === secondElement.missing &&
+        firstElement.range === secondElement.range
       )
     })
-    if (matchingDependencyIndex === -1) {
+    if (!alreadyHas) {
       firstTree.push(secondElement)
-    } else if (secondElement.range !== undefined) {
-      var firstElement = firstTree[matchingDependencyIndex]
-      if (firstElement.range === undefined) {
-        firstElement.range = secondElement.range
-      } else {
-        if (firstElement.range !== secondElement.range) {
-          var error = new Error(
-            'direct-dependency range mismatch ' +
-            'for ' + secondElement.name + '@' + secondElement.version
-          )
-          error.rangeMismatch = true
-          throw error
-        }
-      }
     }
   })
-  return firstTree
+}
+
+function mergeRoots (firstRoot, secondRoot) {
+  secondRoot.links.forEach(function (secondLink) {
+    var alreadyHas = firstRoot.links.some(function (firstLink) {
+      return sameLinks(firstLink, secondLink)
+    })
+    if (alreadyHas) {
+      return
+    }
+    var rangeConflict = firstRoot.links.some(function (firstLink) {
+      return conflictingRanges(firstLink, secondLink)
+    })
+    if (rangeConflict) {
+      var error = new Error(
+        'dependency-range mismatch for ' + secondLink.name
+      )
+      error.rangeMismatch = true
+      throw error
+    }
+    firstRoot.links.push(secondLink)
+  })
+}
+
+var LINK_PROPERTIES = ['name', 'range', 'version']
+
+function sameLinks (firstLink, secondLink) {
+  return LINK_PROPERTIES.every(function (property) {
+    return firstLink[property] === secondLink[property]
+  })
+}
+
+function conflictingRanges (first, second) {
+  return (
+    first.name === second.name &&
+    first.range !== second.range
+  )
+}
+
+function isRoot (element) {
+  return element.root === true
 }
